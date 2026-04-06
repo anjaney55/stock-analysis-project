b2fe217e6ca4d158fc2f61607a72356fb1b77403
anjaney55 <anjaneynaik5@gmail.com>
2026-04-07 01:56:25 +0530

    Update the sql query

--- sql.sql
+++ sql.sql
@@ -4,3 +4,11 @@ CREATE TABLE news (
     clean_title TEXT
 );
 
+SELECT * FROM news;
+
+ALTER TABLE news
+ADD sentiment FLOAT,
+ADD sentiment_label VARCHAR(20);
+
+ALTER Table news
+DROP COLUMN title;
